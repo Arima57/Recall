33041ed206c82b36b6dcfadefb9c4059a313da68
Killswitch <139682255+Arima57@users.noreply.github.com>
2024-10-09 08:18:40 +0600

    Some more Side effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
+
 import Menu from './components/Menu';
 import Page from './pages/Page';
 
